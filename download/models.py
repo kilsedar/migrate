5a5b117594734545287fa44c1b240015434ee908
Candan Eylul Kilsedar <candaneylul.kilsedar@polimi.it>
2016-12-02 12:30:15 +0100

    updated gitignore and download model

--- download/models.py
+++ download/models.py
@@ -8,8 +8,8 @@ class Download(models.Model):
     surname = models.CharField(max_length=20, blank=True, default=None, verbose_name=u'surname')
     email = models.EmailField(max_length=30, blank=False, default=None, verbose_name=u'email')
     affiliation = models.CharField(max_length=500, blank=True, default=None, verbose_name=u'affiliation')
-    project_name = models.CharField(max_length=500, blank=False, default=None, verbose_name=u'name of the project, initiative, study or application')
-    project_description = models.CharField(max_length=1000, blank=False, default=None, verbose_name=u'brief description of the project, initiative, study or application')
+    project_name = models.CharField(max_length=500, blank=False, default=None, verbose_name=u'name of the project, initiative, study or application using MIGRATE data')
+    project_description = models.CharField(max_length=1000, blank=False, default=None, verbose_name=u'brief description of this project, initiative, study or application')
 
     def __unicode__(self):
         return self.name + " " + self.surname
